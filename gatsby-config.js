0f3a7c9f8acffb005bd0680dbd57ef4d637ecf10
Hiroaki WAKATSUKI <mail@puzzel.jp>
2021-03-03 17:09:04 +0900

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: "STUDIO WT",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "30XM73J9LK",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Hello, STUDIO WT.</h1>
+    </main>
+  )
+}
+
+export default IndexPage
